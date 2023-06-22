06b1af54fabfdbde0c22f343d0c77994538e1178
Orestis Ousoultzoglou <xlxs4@protonmail.ch>
2023-06-22 15:58:32 +0300

    Fix dsmc install script, add bc for dsmc

--- dsmc/install.sh
+++ dsmc/install.sh
@@ -14,13 +14,13 @@ speedup=1.0
 if [[ $nProcs -eq 2 || $nProcs -eq 3 ]]
 then
     speedup=1.5
-elif [ $nProcs -ge 4 ]; then
+elif [ "$nProcs" -ge 4 ]; then
     speedup=2.0
 fi
 
 display_bar() {
     local w=20 p=$1; shift
-    local pct=$(( $p*$w/100 ))
+    local pct=$(( p*w/100 ))
     printf -v arrows "%*s" "$pct" ""; arrows=${arrows// />};
     printf "\r\e[K[%-*s] %3d%% %s" "$w" "$arrows" "$p" "$*"; 
 }
@@ -29,14 +29,15 @@ progress_bar() {
     local slp=$1;
     for x in {1..99}
     do
-        display_bar "$x" $2
-        sleep $slp
+        display_bar "$x" "$2"
+        sleep "$slp"
     done
 }
 
 print_status() {
     local logfile="$1"
-    local last_line=$( tail -n 2 $logfile )
+    local last_line
+    last_line=$( tail -n 2 "$logfile" )
     if [[ "$last_line" == *"successfully"* ]]; then
        echo -e "\nSUCCESS";
     else
@@ -45,13 +46,13 @@ print_status() {
 }
 
 install_CFD() {
-    local sleep_period=`bc -l <<< $tref_CFD/$speedup/100`
-    progress_bar $sleep_period "installing CFD module" &
-    ./build/install-CFD.sh $nProcs > logInstall-CFD 2>&1 &
+    local sleep_period
+    sleep_period=$(bc -l <<< $tref_CFD/$speedup/100)
+    progress_bar "$sleep_period" "installing CFD module" &
+    ./build/install-CFD.sh "$nProcs" > logInstall-CFD 2>&1 &
     wait %2
-    if [ ! -z `jobs -p` ]
+    if [ -n "$(jobs -p)" ]
     then
-      progress_bar_pid=$!
       disown
       pkill -P $$  > /dev/null 2>&1
     fi
@@ -60,13 +61,13 @@ install_CFD() {
 }
 
 sync_CFD() {
-    local sleep_period=`bc -l <<< $tref_CFD/$speedup/100`
-    progress_bar $sleep_period "syncing CFD module" &
-    ./build/resync-CFD.sh $nProcs > logSync-CFD 2>&1 &
+    local sleep_period
+    sleep_period=$(bc -l <<< $tref_CFD/$speedup/100)
+    progress_bar "$sleep_period" "syncing CFD module" &
+    ./build/resync-CFD.sh "$nProcs" > logSync-CFD 2>&1 &
     wait %2
-    if [ ! -z `jobs -p` ]
+    if [ -n "$(jobs -p)" ]
     then
-      progress_bar_pid=$!
       disown
       pkill -P $$  > /dev/null 2>&1
     fi
@@ -75,13 +76,13 @@ sync_CFD() {
 }
 
 install_DSMC() {
-    local sleep_period=`bc -l <<< $tref_DSMC/$speedup/100`
-    progress_bar $sleep_period "installing DSMC module" &
-    ./build/install-DSMC.sh $nProcs > logInstall-DSMC 2>&1 &
+    local sleep_period
+    sleep_period=$(bc -l <<< $tref_DSMC/$speedup/100)
+    progress_bar "$sleep_period" "installing DSMC module" &
+    ./build/install-DSMC.sh "$nProcs" > logInstall-DSMC 2>&1 &
     wait %2
-    if [ ! -z `jobs -p` ]
+    if [ -n "$(jobs -p)" ]
     then
-      progress_bar_pid=$!
       disown
       pkill -P $$  > /dev/null 2>&1
     fi
@@ -90,13 +91,13 @@ install_DSMC() {
 }
 
 sync_DSMC() {
-    local sleep_period=`bc -l <<< $tref_DSMC/$speedup/100`
-    progress_bar $sleep_period "syncing DSMC module" &
-    ./build/resync-DSMC.sh $nProcs > logSync-DSMC 2>&1 &
+    local sleep_period
+    sleep_period=$(bc -l <<< $tref_DSMC/$speedup/100)
+    progress_bar "$sleep_period" "syncing DSMC module" &
+    ./build/resync-DSMC.sh "$nProcs" > logSync-DSMC 2>&1 &
     wait %2
-    if [ ! -z `jobs -p` ]
+    if [ -n "$(jobs -p)" ]
     then
-      progress_bar_pid=$!
       disown
       pkill -P $$  > /dev/null 2>&1
     fi
@@ -105,13 +106,13 @@ sync_DSMC() {
 }
 
 install_PICDSMC() {
-    local sleep_period=`bc -l <<< $tref_PICDSMC/$speedup/100`
-    progress_bar $sleep_period "installing hybrid PIC-DSMC module" &
-    ./build/install-hybridPICDSMC.sh $nProcs > logInstall-hybridPICDSMC 2>&1 &
+    local sleep_period
+    sleep_period=$(bc -l <<< $tref_PICDSMC/$speedup/100)
+    progress_bar "$sleep_period" "installing hybrid PIC-DSMC module" &
+    ./build/install-hybridPICDSMC.sh "$nProcs" > logInstall-hybridPICDSMC 2>&1 &
     wait %2
-    if [ ! -z `jobs -p` ]
+    if [ -n "$(jobs -p)" ]
     then
-      progress_bar_pid=$!
       disown
       pkill -P $$  > /dev/null 2>&1
     fi
@@ -120,13 +121,13 @@ install_PICDSMC() {
 }
 
 sync_PICDSMC() {
-    local sleep_period=`bc -l <<< $tref_PICDSMC/$speedup/100`
-    progress_bar $sleep_period "syncing hybrid PIC-DSMC module" &
-    ./build/resync-hybridPICDSMC.sh $nProcs > logSync-hybridPICDSMC 2>&1 &
+    local sleep_period
+    sleep_period=$(bc -l <<< $tref_PICDSMC/$speedup/100)
+    progress_bar "$sleep_period" "syncing hybrid PIC-DSMC module" &
+    ./build/resync-hybridPICDSMC.sh "$nProcs" > logSync-hybridPICDSMC 2>&1 &
     wait %2
-    if [ ! -z `jobs -p` ]
+    if [ -n "$(jobs -p)" ]
     then
-      progress_bar_pid=$!
       disown
       pkill -P $$  > /dev/null 2>&1
     fi
