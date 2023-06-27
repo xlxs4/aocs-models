eab8b1ba44706162bfefd7c3e72789dbde629473
voulgarakisromanos <voulgarakisromanos@gmail.com>
2023-06-27 13:34:17 +0300

    Changed rotation to utils

--- blender/utils.py
+++ blender/utils.py
@@ -13,18 +13,3 @@ def rotation_quaternion_from_vectors(vec1, vec2):
     dot_prod = np.dot(vec1, vec2)
     q = Quaternion(axis=cross_prod, angle=np.arccos(dot_prod))  # Constructing the quaternion
     return q
-
-# Given: v (replace vx, vy, vz with the actual coordinates of your vector)
-v = np.array([1, 1, 1])  
-v = v / np.linalg.norm(v)
-r = np.array([1, 0, 0])
-
-# Compute the rotation quaternion
-rot_quaternion = rotation_quaternion_from_vectors(v, r)
-
-# Apply the rotation quaternion to v
-v_rotated = rot_quaternion.rotate(v)
-
-print(f"The original vector v is {v}")
-print(f"The rotation quaternion is {rot_quaternion}")
-print(f"The rotated vector v_rotated is {v_rotated}")
