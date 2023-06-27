3b0b7fa6566fcc52bfffdd8ac660c25d2418f33c
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-06-27 13:59:25 +0300

    blender: refactor astro utils

--- blender/src/blender.py
+++ blender/src/blender.py
@@ -14,12 +14,6 @@ def get_cross_section(quaternion):
     # Load the .blend file
     bpy.ops.wm.open_mainfile(filepath=filepath)
 
-    # Specify the rotation angles in degrees
-    # rotation_angles = (0, 0, 0)  # rotation in X, Y, Z directions
-
-    # Convert the rotation angles to radians (Blender uses radians for rotation)
-    # rotation_angles_rad = [radians(angle) for angle in rotation_angles]
-
     bpy.data.objects["PeakSat v2"].rotation_quaternion = list(quaternion)
 
     # Specify the output path for the rendered image
@@ -51,15 +45,9 @@ def get_cross_section(quaternion):
         # Binarize the image using a threshold of 0.5
         pixels_array_bin = pixels_array > 100
 
-        # plot.imshow(pixels_array_bin)
-        # plot.show()
-
         # Count the white pixels (pixels over threshold)
         white_pixels = np.sum(pixels_array_bin)
         cross_sectional_area = white_pixels / pixels_per_m2
-
-        # print('Number of white pixels:', white_pixels)
-        print('Cross-sectional area:', cross_sectional_area, "m2")
 
     finally:
         # Clean up the temporary file
