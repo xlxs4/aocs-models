af2def060b225aa4bd946168fbc53f4239b131e0
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-06-27 18:59:22 +0300

    Generate power; don't write render to disk
    Co-authored-by: voulgarakisromanos <voulgarakisromanos@gmail.com>

--- blender/src/blender.py
+++ blender/src/blender.py
@@ -1,57 +1,48 @@
 import bpy
 import numpy as np
 from PIL import Image
-import os
-import tempfile
 
 
 def get_cross_section(quaternion):
-    # Specify the path to your .blend file
-    filepath = "../model/model.blend"
-
+    # FIXME
     pixels_per_m2 = 509953.9170506912
 
-    # Load the .blend file
-    bpy.ops.wm.open_mainfile(filepath=filepath)
+    bpy.data.objects["PeakSat v2"].rotation_mode = 'QUATERNION'
+    bpy.data.objects["PeakSat v2"].rotation_quaternion = tuple(quaternion)
 
-    bpy.data.objects["PeakSat v2"].rotation_quaternion = list(quaternion)
+    # switch on nodes
+    bpy.context.scene.use_nodes = True
+    tree = bpy.context.scene.node_tree
+    links = tree.links
 
-    # Specify the output path for the rendered image
-    bpy.context.scene.render.filepath = "render.png"
+    # create input render layer node
+    rl = tree.nodes.new('CompositorNodeRLayers')
+    rl.location = 185, 285
 
-    # Set output format to PNG
-    bpy.context.scene.render.image_settings.file_format = 'PNG'
+    # create output node
+    v = tree.nodes.new('CompositorNodeViewer')
+    v.location = 750, 210
+    v.use_alpha = False
+
+    # Links
+    links.new(rl.outputs[0], v.inputs[0])  # link Image output to Viewer input
 
     # Specify render resolution
     bpy.context.scene.render.resolution_x = 400
     bpy.context.scene.render.resolution_y = 400
 
     # Render the scene
-    bpy.ops.render.render(write_still=True)
+    bpy.ops.render.render(write_still=False)
 
-    # Create a temporary file
-    fd, path = tempfile.mkstemp(suffix=".png")
+    # copy buffer to numpy array for faster manipulation
+    arr = np.array(bpy.data.images['Viewer Node'].pixels)
+    image = arr.reshape(400, 400, 4)
+    image = np.dot(image[..., :3], [0.299, 0.587, 0.114])
 
-    try:
-        # Save the rendered image to the temporary file
-        bpy.data.images['Render Result'].save_render(filepath=path)
+    # Binarize the image using a threshold of 0.5
+    pixels_array_bin = image > 3.5
 
-        # Open the image file
-        img = Image.open(path)
+    # Count the white pixels (pixels over threshold)
+    white_pixels = np.sum(pixels_array_bin)
 
-        # Convert the image data to a numpy array
-        pixels_array = np.array(img)
-
-        # Binarize the image using a threshold of 0.5
-        pixels_array_bin = pixels_array > 100
-
-        # Count the white pixels (pixels over threshold)
-        white_pixels = np.sum(pixels_array_bin)
-        cross_sectional_area = white_pixels / pixels_per_m2
-
-    finally:
-        # Clean up the temporary file
-        os.close(fd)
-        os.remove(path)
-
-    return cross_sectional_area
+    return white_pixels / pixels_per_m2
