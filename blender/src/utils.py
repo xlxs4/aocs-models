1b50ee32d5f0e60cd35487c4b216fccfdf652725
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-07-14 11:57:04 +0300

    align util refactor

--- blender/src/utils.py
+++ blender/src/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from pyquaternion import Quaternion
 from scipy.spatial.transform import Rotation as R
 
+
 def align_with_sun_and_nadir(v1, v2):
     # Normalize the vectors
     v1 = v1 / np.linalg.norm(v1)  # Sun vector
@@ -9,9 +10,16 @@ def align_with_sun_and_nadir(v1, v2):
 
     # Calculate the satellite body frame
     x_sat = -v1
-    y_sat = v2 - v1 * np.dot(v1, v2) # remove the component of v2 that is in the direction of v1
-    y_sat = y_sat / np.linalg.norm(y_sat) # normalize again after the subtraction
-    z_sat = np.cross(x_sat, y_sat); z_sat = z_sat / np.linalg.norm(z_sat) # z is now perpendicular to both x (v1) and y
+    y_sat = v2 - v1 * np.dot(
+        v1, v2
+    )  # remove the component of v2 that is in the direction of v1
+    y_sat = y_sat / np.linalg.norm(
+        y_sat
+    )  # normalize again after the subtraction
+    z_sat = np.cross(x_sat, y_sat)
+    z_sat = z_sat / np.linalg.norm(
+        z_sat
+    )  # z is now perpendicular to both x (v1) and y
 
     # Form the rotation matrix
     rotation_matrix = np.vstack((x_sat, y_sat, z_sat))
